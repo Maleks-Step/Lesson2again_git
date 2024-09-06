6e9ad39699d899516d889db68415813ff8017528
Alex Mishustina <alex.mishustina@yandex.ru>
2024-09-06 22:25:22 +0300

    added getMessage() method

--- src/main/java/ru/mishustina/Main.java
+++ src/main/java/ru/mishustina/Main.java
@@ -2,7 +2,7 @@ package ru.mishustina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(getMessage());
     }
 
     private static String getMessage() {
