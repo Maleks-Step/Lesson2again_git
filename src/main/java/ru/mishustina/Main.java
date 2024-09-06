0838b2f8d4641b212b4d91e276c13d8729d9371d
Aleks Mishustina <alex.mishustina@yandex.ru>
2024-09-06 22:41:05 +0300

    Update Main.java

--- src/main/java/ru/mishustina/Main.java
+++ src/main/java/ru/mishustina/Main.java
@@ -2,7 +2,7 @@ package ru.mishustina;
 
 public class Main {
 
-    private static String msg = ""Welcome!"";
+    private static String msg = "Welcome!";
     public static void main(String[] args) {
         System.out.println(msg);
     }
